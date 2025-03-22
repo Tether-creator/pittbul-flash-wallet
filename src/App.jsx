6b4a17f82991932de7e1a22473b2903e67509a60
Tether-creator <gruthzillions@gmail.com>
2025-03-22 11:51:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="min-h-screen w-full bg-black text-white flex justify-center items-start p-6">
-      <div className="w-full max-w-5xl">
+      <div className="w-full max-w-full">
         <h1 className="text-4xl font-bold text-center mb-10">Pittbul Flash Wallet</h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
