3ebb2f4a55ebb61d982a1f1be9e411e7f0fe78ce
Tether-creator <gruthzillions@gmail.com>
2025-03-22 16:04:21 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; 
+import './index.css';
+
+// 🔐 Import Web3 Wallet Provider
+import { WalletProvider } from './web3/WalletProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WalletProvider>
+      <App />
+    </WalletProvider>
   </React.StrictMode>
 );
